8c37067852a972201e7c3fa64eb3cb3ede4bff8c
Alexander <mega.medved87@gmail.com>
2023-08-11 20:14:24 -0500

    Add: Usestate to Todo.js

--- src/Modal.js
+++ src/Modal.js
@@ -1,9 +1,12 @@
-export default function Modal() {
+
+
+export default function Modal(props) {
+
     return(
         <div className="modal">
             <p>Are you sure?</p>
-            <button className="btn .btn--alt">Cancel</button>
-            <button className="btn">Confrim</button>
+            <button className="btn .btn--alt" onClick={props.onCancel}>Cancel</button>
+            <button className="btn" onClick={props.onConfrim}>Confrim</button>
         </div>
     );
 }
--- src/Todo.js
+++ src/Todo.js
@@ -1,8 +1,17 @@
+import {useState} from 'react';
+import Modal from './Modal'
+import Backdrop from './Backdrop';
+
 export default function Todo(props) {
- function deleteHandler() {
-  console.log(props.text);
+  const [modalItsOpen, setModalItsOpen] = useState(false);
+  
+  function deleteHandler() {
+    setModalItsOpen(true);
  }
 
+ function closeModalHandler(){
+    setModalItsOpen(false);
+ }
 
 
   return(
@@ -10,9 +19,15 @@ export default function Todo(props) {
         <div className='card'>
           <h2>{props.text}</h2>
           <div className='atcions'>
-            <button className='btn' onClick={deleteHandler()}>Delete</button>
+            <button className='btn' onClick={deleteHandler}>
+               Delete
+            </button>
           </div>
        </div>
+        {modalItsOpen && (
+        <Modal onCancel={closeModalHandler} onConfrim={closeModalHandler} />
+        )}
+        {modalItsOpen && <Backdrop onCancel={closeModalHandler} />}
     </div>  
     );
 }
