cc4f2c6b32ec9c3a859f0acf29010d351bd066f3
Alexander <mega.medved87@gmail.com>
2023-08-21 14:55:47 -0500

    build: layout (MainNavigation.js,css)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './index.css';
 // import Todo from './Todo';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route} from "react-router-dom";
 
 import AllMeetupsPage from './pages/AllMeetups';
 import FavouritePage from './pages/Favourite';
 import NewMeetup from './pages/NewMeetup';
+import MainNavigation from './components/layout/MainNavigation';
 
 
 function App() {
@@ -18,14 +19,12 @@ function App() {
 {/* <Routes>
    <Route path="/all-meetups"> <AllMeetupsPage /></Route>
 </Routes> */}
-
-    <Routes>
-      <Route path="/" element={<AllMeetupsPage />}></Route>
-    </Routes>
-
-
-
-
+          <MainNavigation/>
+          <Routes>
+            <Route path="/" element={<AllMeetupsPage />}></Route>
+            <Route path="/favourites" element={<FavouritePage />}></Route>
+            <Route path="/new-meetup" element={<NewMeetup />}></Route>
+          </Routes>
      </div>
   );
 }
---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,25 @@
+import {Link} from "react-router-dom";
+
+export default function MainNavigation () {
+    return (
+       <header>
+        <div>React Meetups</div>
+        <nav>
+            <ul>
+                <li>
+                    {/* It's Link istead of a(in HTML) */}
+                    <Link to='/'>All Meetups</Link>
+                </li>
+                <li>
+                    {/* It's Link istead of a(in HTML) */}
+                    <Link to='/new-meetup'>Add new Meetup</Link>
+                </li>
+                <li>
+                    {/* It's Link istead of a(in HTML) */}
+                    <Link to='/favourites'>Favourites MeetUp</Link>
+                </li>
+            </ul>
+        </nav>
+       </header>
+    )
+}
