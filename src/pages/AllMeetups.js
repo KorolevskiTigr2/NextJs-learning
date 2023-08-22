3325482ec1b08e8f16f59192bc0d90af1a4db800
Alexander <mega.medved87@gmail.com>
2023-08-22 12:58:12 -0500

    build: array in AllMeetups.js

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,6 +1,35 @@
+import { Link } from "react-router-dom";
+
+const DUMMY_DATE = [
+  {
+    id: 'm1',
+    title: 'This is a first meetup',
+    image: '#',
+    address: '#',
+    description: 
+      'This is a first, amazing meetup'
+  },
+  {
+    id: 'm2',
+    title: 'This is a second meetup',
+    image: '#',
+    address: '#',
+    description: 
+      'This is a second, amazing meetup'
+  }
+]
+ 
+ 
  function AllMeetupsPage() {
-  return (
-    <div>AllMeetupsPage</div>
+  return ( 
+   <div>
+    <ul>
+    <h1>AllMeetups</h1>
+    {DUMMY_DATE.map((meetup) => {
+      return <li key = {meetup.id}>{meetup.title}</li>
+    })}
+    </ul>
+    </div>
   )
  }
 
