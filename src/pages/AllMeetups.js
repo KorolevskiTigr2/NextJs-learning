067c2a2d7eaafefbf944c237841fc2cf3e9a3715
Alexander <mega.medved87@gmail.com>
2023-08-22 13:29:13 -0500

    buid: basic structure of website need to fix: image

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,19 +1,19 @@
-import { Link } from "react-router-dom";
+import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_DATE = [
   {
     id: 'm1',
     title: 'This is a first meetup',
-    image: '#',
-    address: '#',
+    image: 'https://www.pexels.com/photo/view-of-street-from-a-glass-window-531880/',
+    address: 'Dom Pyshkina',
     description: 
       'This is a first, amazing meetup'
   },
   {
     id: 'm2',
     title: 'This is a second meetup',
-    image: '#',
-    address: '#',
+    image: 'https://pexels.com/photo/high-angle-view-of-cityscape-against-cloudy-sky-313782/',
+    address: 'Dom Esenina',
     description: 
       'This is a second, amazing meetup'
   }
@@ -22,14 +22,9 @@ const DUMMY_DATE = [
  
  function AllMeetupsPage() {
   return ( 
-   <div>
-    <ul>
-    <h1>AllMeetups</h1>
-    {DUMMY_DATE.map((meetup) => {
-      return <li key = {meetup.id}>{meetup.title}</li>
-    })}
-    </ul>
-    </div>
+   <section>
+      <MeetupList meetups={DUMMY_DATE}/>
+    </section>
   )
  }
 
