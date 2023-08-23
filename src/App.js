f0720ca9f3a1d52e356c7654679693183f6ea49e
Alexander <mega.medved87@gmail.com>
2023-08-23 13:33:05 -0500

    style: App.js has been apdated

--- src/App.js
+++ src/App.js
@@ -5,18 +5,19 @@ import { Routes, Route} from "react-router-dom";
 import AllMeetupsPage from './pages/AllMeetups';
 import FavouritePage from './pages/Favourite';
 import NewMeetup from './pages/NewMeetup';
-import MainNavigation from './components/layout/MainNavigation';
+import Layout from './components/layout/Layout';
 
 
 function App() {
   return (
     <div>
-          <MainNavigation/>
+        <Layout>
           <Routes>
             <Route path="/" element={<AllMeetupsPage />}></Route>
             <Route path="/favourites" element={<FavouritePage />}></Route>
             <Route path="/new-meetup" element={<NewMeetup />}></Route>
           </Routes>
+        </Layout>
      </div>
   );
 }
